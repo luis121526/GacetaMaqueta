9a840bd569b36e787636f3fc3cd98b6114b69049
Luis <diazdiaz121526>
2021-07-27 12:37:25 -0500

    cambios

--- Contenidoweb/guadalajara.php
+++ Contenidoweb/guadalajara.php
@@ -58,11 +58,11 @@ if(!isset($usuario)){
     <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">Locaciones</a>
     <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu" style="background-color: #EBF9F8">     
-        <li class="dropdown-item"><a href="#">Cancún/Rivera Maya</a></li>
-        <li class="dropdown-item"><a href="#">CDMX</a></li>
-        <li class="dropdown-item"><a href="#">Guadalajara</a></li>
-        <li class="dropdown-item"><a href="#">Monterrey</a></li>
-        <li class="dropdown-item"><a href="#">Queretaro</a></li>
+    <li class="dropdown-item"><a href="Contenidoweb/cancun.php">Cancún/Rivera Maya</a></li>
+        <li class="dropdown-item"><a href="Contenidoweb/cdmx.php">CDMX</a></li>
+        <li class="dropdown-item"><a href="Contenidoweb/guadalajara.php">Guadalajara</a></li>
+        <li class="dropdown-item"><a href="Contenidoweb/monterrey.php">Monterrey</a></li>
+        <li class="dropdown-item"><a href="Contenidoweb/queretaro.php">Queretaro</a></li>
       </ul>
     </li>
     <li class="nav-item">
